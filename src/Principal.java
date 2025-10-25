4911e0ad7691b0343fcddf2efcc2f41893ab65f8
Martin de Jesus Londoño Echavarria <martin0332@gmail.com>
2025-10-25 13:56:25 -0500

    Agregamos constructor a las clases Titulo, Pelicula y Serie) git status back exit

--- src/Principal.java
+++ src/Principal.java
@@ -8,9 +8,8 @@ import java.util.ArrayList;
 
 public class Principal {
     public static void main(String[] args) {
-        Pelicula miPelicula = new Pelicula();
-        miPelicula.setNombre("Encanto");
-        miPelicula.setFechaDeLanzamiento(2021);
+        //constructor de la clase película: crea espacio en memoria
+        Pelicula miPelicula = new Pelicula("Encanto",2021);
         miPelicula.setDuracionEnMinutos(180);
         //System.out.println("Duración de la película: " + miPelicula.getDuracionEnMinutos());
 
@@ -22,18 +21,14 @@ public class Principal {
        // System.out.println(miPelicula.calculaMediaEvaluaciones());
 
 
-        Serie lost = new Serie();
-        lost.setNombre("Lost");
-        lost.setFechaDeLanzamiento(2000);
+        Serie lost = new Serie("Lost",2000);
         //lost.muestraFichaTecnica();
         lost.setTemporadas(10);
         lost.setEpisodiosPorTemporada(10);
         lost.setMinutosPorEpisodio(50);
         //System.out.println("Duracion de la série: " + lost.getDuracionEnMinutos());
 
-        Pelicula otraPelicula = new Pelicula();
-        otraPelicula.setNombre("Avatar");
-        otraPelicula.setFechaDeLanzamiento(2023);
+        Pelicula otraPelicula = new Pelicula("Avatar",2023);
         otraPelicula.setDuracionEnMinutos(200);
 
         CalculadoraDeTiempo calculadora = new CalculadoraDeTiempo();
@@ -51,9 +46,7 @@ public class Principal {
         episodio.setTotalVisualizaciones(300);
         filtro.filtra(episodio);
 
-        var peliculaDeBruno = new Pelicula();
-        peliculaDeBruno.setNombre("EL señor de los anillos");
-        peliculaDeBruno.setDuracionEnMinutos(180);
+        var peliculaDeBruno = new Pelicula("EL señor de los anillos",180);
         peliculaDeBruno.setFechaDeLanzamiento(2001);
 
         //Crear un array
@@ -64,10 +57,9 @@ public class Principal {
 
         System.out.println("Tamaño de la lista: " + listaDePeliculas.size());
         System.out.println("La primera pelicula es: " + listaDePeliculas.get(0).getNombre());
-
-        System.out.println(listaDePeliculas);
-
+        System.out.println(listaDePeliculas.toString());
         System.out.println("toString de la pelicula: " + listaDePeliculas.get(0).toString());
+
 
     }
 }
